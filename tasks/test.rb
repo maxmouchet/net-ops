89cd151a8d86197182aa972be34b0ce16a12f503
Maxime Mouchet <mouchet.max@gmail.com>
2013-07-18 20:35:22 -0400

    Credentials fixed in test.rb

--- tasks/test.rb
+++ tasks/test.rb
@@ -1,17 +1,21 @@
 require './downcase_hostname_task'
 
-hosts = %w()
+hosts = %w( 192.168.0.104 )
+
+credentials = YAML.load_file('credentials.yml')
+
 
 options     = { timeout: 30, prompt: /.+(#|>)/ }
-credentials = { username: '', password: '' }
+credentials = { username: credentials.fetch('username'),
+                password: credentials.fetch('username') }
 
 pool = Thread.pool(10)
 
 hosts.each do |host|
-  pool.process do
+  # pool.process do
     t = DowncaseHostnameTask.new(host, options, credentials)
     t.work
-  end
+  # end
 end
 
 pool.shutdown
